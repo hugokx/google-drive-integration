c5ac796cf146ec17e32441a71039f4af3bb2da9c
hugokx <hugues.henrion@gmail.com>
2024-09-11 11:45:26 +0400

    update js

--- js/google-drive-integration.js
+++ js/google-drive-integration.js
@@ -1,42 +1,62 @@
-// File: js/google-drive-integration.js
+// File: wp-content/plugins/google-drive-integration/js/google-drive-integration.js
 
-let gapi;
+let gapiLoaded = false;
+let gapiInitialized = false;
+
+function loadGapiAndInit() {
+    if (typeof gapi === 'undefined') {
+        // Load the Google API script if it's not already loaded
+        const script = document.createElement('script');
+        script.src = 'https://apis.google.com/js/api.js';
+        script.onload = function() {
+            gapiLoaded = true;
+            initGoogleDriveAPI();
+        };
+        document.head.appendChild(script);
+    } else {
+        gapiLoaded = true;
+        initGoogleDriveAPI();
+    }
+}
 
 function initGoogleDriveAPI() {
-    gapi.load('client:auth2', () => {
+    if (!gapiLoaded) return;
+    
+    gapi.load('client', () => {
         gapi.client.init({
+            apiKey: googleDriveIntegration.apiKey,
             clientId: googleDriveIntegration.clientId,
+            discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
             scope: 'https://www.googleapis.com/auth/drive.readonly'
         }).then(() => {
-            // API is initialized and ready
+            gapiInitialized = true;
             console.log('Google Drive API initialized');
+            // Dispatch an event to notify that GAPI is ready
+            document.dispatchEvent(new Event('gapi-loaded'));
+        }).catch(error => {
+            console.error('Error initializing Google Drive API:', error);
         });
     });
 }
 
 function listFolderContents(folderId) {
-    return gapi.client.drive.files.list({
-        q: `'${folderId}' in parents`,
-        fields: 'files(id, name, mimeType, webViewLink)'
-    }).then(response => {
-        return response.result.files;
-    });
-}
-
-// Function to be called from PHP
-function getDriveFolderContents(folderId) {
     return new Promise((resolve, reject) => {
-        gapi.auth2.getAuthInstance().signIn().then(() => {
-            listFolderContents(folderId).then(files => {
-                resolve(files);
-            }).catch(error => {
-                reject(error);
-            });
-        }).catch(error => {
-            reject(error);
-        });
+        if (!gapiInitialized) {
+            reject('Google API not initialized');
+            return;
+        }
+        
+        gapi.client.drive.files.list({
+            q: `'${folderId}' in parents`,
+            fields: 'files(id, name, mimeType, webContentLink)'
+        }).then(response => {
+            resolve(response.result.files);
+        }).catch(reject);
     });
 }
 
-// Initialize the API when the script loads
-initGoogleDriveAPI();
+// Start loading GAPI when the script runs
+loadGapiAndInit();
+
+// Expose the listFolderContents function globally
+window.listFolderContents = listFolderContents;
